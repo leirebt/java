058429e9e4fdb304cddbedd2ca64c1127b6792b1
leirebt <leire.bt@gmail.com>
2023-11-14 07:53:26 +0100

    Patrones de diseño Observer

--- PatronesDeDiseno/Comportamiento/Observer/EjemploObserver.java
+++ PatronesDeDiseno/Comportamiento/Observer/EjemploObserver.java
@@ -2,9 +2,9 @@ package Proyectos.PatronesDeDiseno.Comportamiento.Observer;
 
 public class EjemploObserver {
     public static void main(String[] args) {
-        //Creamos la empresa.
+
         Corporacion google = new Corporacion("Google", 1000);
-        //Creamos a los inversionistas de la empresa que sería los observadores.
+
         google.addObserver(observable -> {
             System.out.println("Paul: "+ observable);
         });
